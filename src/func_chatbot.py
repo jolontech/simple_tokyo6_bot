af8609d7aad2209ed501155c6b5020de7efa6f65
Jolon <jolon@Jolons-MacBook-Pro.local>
2025-06-24 03:52:25 +0900

    Messages in Karin and Chifuyu ver were added

--- src/func_chatbot.py
+++ src/func_chatbot.py
@@ -61,10 +61,10 @@ def get_message_dict(narrator="rikka"):
   if narrator == "rikka":
     message_for_hello = load_response('response/rikka/messages/for_hello.txt')
     kaomoji_for_hello = load_response('response/rikka/kaomoji/for_hello.txt')
-    message_for_broccoli = load_response('response/rikka/messages/for_broccoli.txt')
-    kaomoji_for_broccoli = load_response('response/rikka/kaomoji/for_broccoli.txt')
-    message_for_yogurt = load_response('response/rikka/messages/for_yogurt.txt')
-    kaomoji_for_yogurt = load_response('response/rikka/kaomoji/for_yogurt.txt')
+    message_for_liked = load_response('response/rikka/messages/for_liked.txt')
+    kaomoji_for_liked = load_response('response/rikka/kaomoji/for_liked.txt')
+    message_for_disliked = load_response('response/rikka/messages/for_disliked.txt')
+    kaomoji_for_disliked = load_response('response/rikka/kaomoji/for_disliked.txt')
     message_for_positive = load_response('response/rikka/messages/for_positive.txt')
     kaomoji_for_positive = load_response('response/rikka/kaomoji/for_positive.txt')
     message_for_negative = load_response('response/rikka/messages/for_negative.txt')
@@ -72,19 +72,19 @@ def get_message_dict(narrator="rikka"):
     message_for_byebye = load_response('response/rikka/messages/for_byebye.txt')
     kaomoji_for_byebye = load_response('response/rikka/kaomoji/for_byebye.txt')
     message_dict = {'hello': message_for_hello, 'positive': message_for_positive,
-                    'negative': message_for_negative, 'yogurt': message_for_yogurt,
-                    'broccoli': message_for_broccoli, 'byebye': message_for_byebye}
+                    'negative': message_for_negative, 'liked': message_for_liked,
+                    'disliked': message_for_disliked, 'byebye': message_for_byebye}
     kaomoji_dict = {'hello': kaomoji_for_hello, 'positive': kaomoji_for_positive,
-                    'negative': kaomoji_for_negative, 'yogurt': kaomoji_for_yogurt,
-                    'broccoli': kaomoji_for_broccoli, 'byebye': message_for_byebye}
+                    'negative': kaomoji_for_negative, 'liked': kaomoji_for_liked,
+                    'disliked': kaomoji_for_disliked, 'byebye': message_for_byebye}
 
   elif narrator == "karin":
     message_for_hello = load_response('response/karin/messages/for_hello.txt')
     kaomoji_for_hello = load_response('response/karin/kaomoji/for_hello.txt')
-    message_for_broccoli = load_response('response/karin/messages/for_broccoli.txt')
-    kaomoji_for_broccoli = load_response('response/karin/kaomoji/for_broccoli.txt')
-    message_for_yogurt = load_response('response/karin/messages/for_yogurt.txt')
-    kaomoji_for_yogurt = load_response('response/karin/kaomoji/for_yogurt.txt')
+    message_for_liked = load_response('response/karin/messages/for_liked.txt')
+    kaomoji_for_liked = load_response('response/karin/kaomoji/for_liked.txt')
+    message_for_disliked = load_response('response/karin/messages/for_disliked.txt')
+    kaomoji_for_disliked = load_response('response/karin/kaomoji/for_disliked.txt')
     message_for_positive = load_response('response/karin/messages/for_positive.txt')
     kaomoji_for_positive = load_response('response/karin/kaomoji/for_positive.txt')
     message_for_negative = load_response('response/karin/messages/for_negative.txt')
@@ -92,19 +92,19 @@ def get_message_dict(narrator="rikka"):
     message_for_byebye = load_response('response/karin/messages/for_byebye.txt')
     kaomoji_for_byebye = load_response('response/karin/kaomoji/for_byebye.txt')
     message_dict = {'hello': message_for_hello, 'positive': message_for_positive,
-                    'negative': message_for_negative, 'yogurt': message_for_yogurt,
-                    'broccoli': message_for_broccoli, 'byebye': message_for_byebye}
+                    'negative': message_for_negative, 'liked': message_for_liked,
+                    'disliked': message_for_disliked, 'byebye': message_for_byebye}
     kaomoji_dict = {'hello': kaomoji_for_hello, 'positive': kaomoji_for_positive,
-                    'negative': kaomoji_for_negative, 'yogurt': kaomoji_for_yogurt,
-                    'broccoli': kaomoji_for_broccoli, 'byebye': message_for_byebye}
+                    'negative': kaomoji_for_negative, 'liked': kaomoji_for_liked,
+                    'disliked': kaomoji_for_disliked, 'byebye': message_for_byebye}
 
   elif narrator == "chifuyu":
     message_for_hello = load_response('response/chifuyu/messages/for_hello.txt')
     kaomoji_for_hello = load_response('response/chifuyu/kaomoji/for_hello.txt')
-    message_for_broccoli = load_response('response/chifuyu/messages/for_broccoli.txt')
-    kaomoji_for_broccoli = load_response('response/chifuyu/kaomoji/for_broccoli.txt')
-    message_for_yogurt = load_response('response/chifuyu/messages/for_yogurt.txt')
-    kaomoji_for_yogurt = load_response('response/chifuyu/kaomoji/for_yogurt.txt')
+    message_for_liked = load_response('response/chifuyu/messages/for_liked.txt')
+    kaomoji_for_liked = load_response('response/chifuyu/kaomoji/for_liked.txt')
+    message_for_disliked = load_response('response/chifuyu/messages/for_disliked.txt')
+    kaomoji_for_disliked = load_response('response/chifuyu/kaomoji/for_disliked.txt')
     message_for_positive = load_response('response/chifuyu/messages/for_positive.txt')
     kaomoji_for_positive = load_response('response/chifuyu/kaomoji/for_positive.txt')
     message_for_negative = load_response('response/chifuyu/messages/for_negative.txt')
@@ -112,11 +112,11 @@ def get_message_dict(narrator="rikka"):
     message_for_byebye = load_response('response/chifuyu/messages/for_byebye.txt')
     kaomoji_for_byebye = load_response('response/chifuyu/kaomoji/for_byebye.txt')
     message_dict = {'hello': message_for_hello, 'positive': message_for_positive,
-                    'negative': message_for_negative, 'yogurt': message_for_yogurt,
-                    'broccoli': message_for_broccoli, 'byebye': message_for_byebye}
+                    'negative': message_for_negative, 'liked': message_for_liked,
+                    'disliked': message_for_disliked, 'byebye': message_for_byebye}
     kaomoji_dict = {'hello': kaomoji_for_hello, 'positive': kaomoji_for_positive,
-                    'negative': kaomoji_for_negative, 'yogurt': kaomoji_for_yogurt,
-                    'broccoli': kaomoji_for_broccoli, 'byebye': message_for_byebye}
+                    'negative': kaomoji_for_negative, 'liked': kaomoji_for_liked,
+                    'disliked': kaomoji_for_disliked, 'byebye': message_for_byebye}
 
   else:
     sys.exit("Error: Inproper args of narrator in get_message_dict function")
@@ -128,23 +128,27 @@ def get_message_dict(narrator="rikka"):
 def comfort_bot_rikka(input_text, sent_analysis_model, message_dict, kaomoji_dict):
   tokens = tokenizer.tokenize(input_text)
 
-  # トークンごとにルールをチェック
+  # 特殊メッセージに該当するならば専用の応答を返す
   for token in tokens:
+    # hello
     if token.surface in "おはよう":
       text = random.choice(message_dict['hello'])
       kaomoji = random.choice(kaomoji_dict['hello'])
       return {"text": text, "kaomoji": kaomoji}
-    
-    elif token.surface in "ブロッコリー":
-      text = random.choice(message_dict['broccoli'])
-      kaomoji = random.choice(kaomoji_dict['broccoli'])
-      return {"text": text, "kaomoji": kaomoji}
-    
+
+    # liked
     elif token.surface in "ヨーグルト":
-      text = random.choice(message_dict['yogurt'])
-      kaomoji = random.choice(kaomoji_dict['yogurt'])
+      text = random.choice(message_dict['liked'])
+      kaomoji = random.choice(kaomoji_dict['liked'])
       return {"text": text, "kaomoji": kaomoji}
-    
+
+    # disliked
+    elif token.surface in "ブロッコリー":
+      text = random.choice(message_dict['disliked'])
+      kaomoji = random.choice(kaomoji_dict['disliked'])
+      return {"text": text, "kaomoji": kaomoji}
+
+  # それ以外なら感情推定に基づいて応答を返す
   sentiment, score = detect_negative_sentiment(input_text, sent_analysis_model)
   if sentiment == "negative":
     text = random.choice(message_dict['negative'])
@@ -164,23 +168,27 @@ def comfort_bot_rikka(input_text, sent_analysis_model, message_dict, kaomoji_dic
 def comfort_bot_karin(input_text, sent_analysis_model, message_dict, kaomoji_dict):
   tokens = tokenizer.tokenize(input_text)
   
-  # トークンごとにルールをチェック
+  # 特殊メッセージに該当するならば専用の応答を返す
   for token in tokens:
+    # hello
     if token.surface in "おはよう":
       text = random.choice(message_dict['hello'])
       kaomoji = random.choice(kaomoji_dict['hello'])
       return {"text": text, "kaomoji": kaomoji}
-    
-    elif token.surface in "ブロッコリー":
-      text = random.choice(message_dict['broccoli'])
-      kaomoji = random.choice(kaomoji_dict['broccoli'])
+
+    # liked
+    elif token.surface in "フロマージュ":
+      text = random.choice(message_dict['liked'])
+      kaomoji = random.choice(kaomoji_dict['liked'])
       return {"text": text, "kaomoji": kaomoji}
-    
-    elif token.surface in "ヨーグルト":
-      text = random.choice(message_dict['yogurt'])
-      kaomoji = random.choice(kaomoji_dict['yogurt'])
+
+    # disliked
+    elif token.surface in "抹茶":
+      text = random.choice(message_dict['disliked'])
+      kaomoji = random.choice(kaomoji_dict['disliked'])
       return {"text": text, "kaomoji": kaomoji}
-  
+
+  # それ以外なら感情推定に基づいて応答を返す
   sentiment, score = detect_negative_sentiment(input_text, sent_analysis_model)
   if sentiment == "negative":
     text = random.choice(message_dict['negative'])
@@ -200,23 +208,27 @@ def comfort_bot_karin(input_text, sent_analysis_model, message_dict, kaomoji_dic
 def comfort_bot_chifuyu(input_text, sent_analysis_model, message_dict, kaomoji_dict):
   tokens = tokenizer.tokenize(input_text)
   
-  # トークンごとにルールをチェック
+  # 特殊メッセージに該当するならば専用の応答を返す
   for token in tokens:
+    # hello
     if token.surface in "おはよう":
       text = random.choice(message_dict['hello'])
       kaomoji = random.choice(kaomoji_dict['hello'])
       return {"text": text, "kaomoji": kaomoji}
-    
-    elif token.surface in "ブロッコリー":
-      text = random.choice(message_dict['broccoli'])
-      kaomoji = random.choice(kaomoji_dict['broccoli'])
+
+    # liked
+    elif token.surface in "抹茶":
+      text = random.choice(message_dict['liked'])
+      kaomoji = random.choice(kaomoji_dict['liked'])
       return {"text": text, "kaomoji": kaomoji}
     
-    elif token.surface in "ヨーグルト":
-      text = random.choice(message_dict['yogurt'])
-      kaomoji = random.choice(kaomoji_dict['yogurt'])
+    # disliked
+    elif token.surface in "スイーツ":
+      text = random.choice(message_dict['disliked'])
+      kaomoji = random.choice(kaomoji_dict['disliked'])
       return {"text": text, "kaomoji": kaomoji}
     
+  # それ以外なら感情推定に基づいて応答を返す
   sentiment, score = detect_negative_sentiment(input_text, sent_analysis_model)
   if sentiment == "negative":
     text = random.choice(message_dict['negative'])
